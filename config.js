8fd9b6e6cbc2efe3b9fcd566aea7e09d64c25f4a
brauliomoralesllamas-alt <braulio.morales.llamas@gmail.com>
2026-04-28 12:52:46 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const MOTTO_CONFIG = {
+    isSoldOut: false,
+    startHour: 14,
+    endHour: 20,
+    precioBowl: 170, // <-- Actualizado a 170
+    descuentoBienvenida: 30, // <-- El descuento de primer cliente
+    scriptURL: 'https://script.google.com/macros/s/AKfycbzJBe79JwZMFDS6g3BnIcHbws5YMhhXjZrngL7t64YTdRef_gfLUf0kmXwebCvKgviaKQ/exec'
+};
